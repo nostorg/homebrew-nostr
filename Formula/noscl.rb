e108b81f8be066a9da4a5ca9a41bc2122b15aad2
1babo <1babo@proton.me>
2023-02-16 14:10:50 -0800

    noscl 0.6.0 (new formula)

---
+++ Formula/noscl.rb
@@ -0,0 +1,22 @@
+class Noscl < Formula
+  desc "Command-line client for Nostr written in Go"
+  homepage "https://nostr.com/"
+  url "https://github.com/fiatjaf/noscl.git",
+      tag:      "v0.6.0",
+      revision: "b4a7e31e09967548d99d477bc6079c43d895e91c"
+  license :public_domain
+  head "https://github.com/fiatjaf/noscl.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "mod", "tidy"
+    system "go", "build"
+    bin.install "noscl"
+  end
+
+  test do
+    system "#{bin}/noscl", "-h"
+  end
+end
