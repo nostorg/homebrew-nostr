8ae3416df7d36261647f59a6fcedf23a18a6ef17
1babo <1babo@proton.me>
2023-02-16 14:08:34 -0800

    nostr-console 0.3.5-beta (new formula)

---
+++ Formula/nostr-console.rb
@@ -0,0 +1,21 @@
+class NostrConsole < Formula
+  desc "Command-line client for Nostr using Dart"
+  homepage "https://github.com/vishalxl/nostr_console"
+  url "https://github.com/vishalxl/nostr_console.git",
+      tag:      "v0.3.5-beta",
+      revision: "00125a3b6b2569c4143eb9c09b351fea32e6f851"
+  license "AGPL-3.0-only"
+  head "https://github.com/vishalxl/nostr_console.git", branch: "main"
+
+  depends_on "dart-sdk" => :build
+
+  def install
+    system "dart", "pub", "get"
+    system "dart", "compile", "exe", "bin/nostr_console.dart"
+    bin.install "bin/nostr_console.exe" => "nostr_console"
+  end
+
+  test do
+    assert_equal version.to_s, shell_output("#{bin}/nostr_console --version").chomp
+  end
+end
