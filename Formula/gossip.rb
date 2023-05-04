ca315e0de3df27c070ac082e9b06fb4b36bb82c1
1babo <1babo@proton.me>
2023-05-04 11:44:23 -0700

    gossip 0.6.0

--- Formula/gossip.rb
+++ Formula/gossip.rb
@@ -2,8 +2,8 @@ class Gossip < Formula
   desc "Desktop client for Nostr written in Rust"
   homepage "https://github.com/mikedilger/gossip"
   url "https://github.com/mikedilger/gossip.git",
-      tag:      "v0.5.2",
-      revision: "cfcbc2d52c87d2ea1bd85360a2a864042624ec19"
+      tag:      "v0.6.0",
+      revision: "b9b1cf132e00e7ec852c45f449ff27597120a01c"
   license "MIT"
   head "https://github.com/mikedilger/gossip.git", branch: "master"
 
