f6e1ed1983730b094c2f3765a536272023b6de70
1babo <1babo@proton.me>
2023-07-07 22:02:23 -0700

    nostur 1.2.0 (new cask)

---
+++ Casks/nostur.rb
@@ -0,0 +1,21 @@
+cask "nostur" do
+  version "1.2.0"
+  sha256 "13e585dbde3f76aeb58b3a8fa97a1e7917bfd3b9e0cca265210dae0bc3deee9b"
+
+  url "https://nostur.com/Nostur-#{version}.dmg"
+  name "Nostur"
+  desc "A nostr client for iPhone and macOS"
+  homepage "https://nostur.com/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/Nostur-(\d+(?:\.\d+)*)\.dmg}i)
+  end
+
+  app "Nostur.app"
+
+  zap trash: [
+    "~/Library/Containers/nostur.com.Nostur",
+  ]
+end
