16bfc25162c1439b6c3aee62fcc420cfc8e5f7e8
1babo <1babo@proton.me>
2023-07-07 22:01:23 -0700

    prettygood 0.1.2-alpha (new cask)

---
+++ Casks/prettygood.rb
@@ -0,0 +1,37 @@
+cask "prettygood" do
+  arch arm: "-arm64"
+
+  version "0.1.2-alpha"
+  sha256 arm:   "9d6616f4056dc74862751d160464d4826388e3da7bc56b044e2810b695da33a6",
+         intel: "a810932b3df9dddd832604502e472ee2f0d0c68491d9fe777f268c7edfecb705"
+
+  url "https://github.com/wds4/pretty-good/releases/download/v0.1.2-alpha/PrettyGood-#{version}#{arch}.dmg"
+  name "PrettyGood"
+  desc "nostr desktop client, motivated by the desire to build a decentralized web of trust"
+  homepage "https://github.com/wds4/pretty-good"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "PrettyGood.app"
+
+  preflight do
+    system_command "xattr",
+                   args: ["-rd", "com.apple.quarantine", "#{staged_path}/PrettyGood.app"]
+  end
+
+  zap trash: [
+    "~/Library/Preferences/org.erb.PrettyGood.plist",
+    "~/Library/Saved Application State/org.erb.PrettyGood.savedState",
+  ]
+
+  caveats <<~EOS
+    This app is not signed with an Apple Developer certificate. Therefore in order
+    to run it may be necessary to override the macOS security policy. This can be
+    done with homebrew using the `--no-quarantine` option:
+
+      brew reinstall --cask --no-quarantine #{token}
+  EOS
+end
