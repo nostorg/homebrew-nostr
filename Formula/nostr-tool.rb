043542e2e10780359e7a06ab1e688867f3efda1a
1babo <1babo@proton.me>
2023-07-12 15:42:48 -0700

    nostr-tool 0.4.0 (new formula)

---
+++ Formula/nostr-tool.rb
@@ -0,0 +1,15 @@
+class NostrTool < Formula
+  desc "Simple CLI tool to send nostr events"
+  homepage "https://github.com/0xtrr/nostr-tool"
+  url "https://github.com/0xtrr/nostr-tool.git",
+      tag:      "0.4.0",
+      revision: "adecf08e69371b63b8a877385773dfa170559451"
+  license "MIT"
+  head "https://github.com/0xtrr/nostr-tool.git", branch: "master"
+
+  depends_on "homebrew/core/rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+end
