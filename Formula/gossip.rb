f3c6643b82657aaf7f335eabbcbd948e60ed46b6
1babo <1babo@proton.me>
2023-08-29 16:53:22 -0700

    gossip formula: added missing features
    Inspired by <https://github.com/jacany/homebrew-gossip>
    Thanks!

--- Formula/gossip.rb
+++ Formula/gossip.rb
@@ -7,9 +7,30 @@ class Gossip < Formula
   license "MIT"
   head "https://github.com/mikedilger/gossip.git", branch: "master"
 
-  depends_on "homebrew/core/rust" => :build
+  option "without-cjk", "Compile without CJK (Chinese, Japanese, and Korean) character sets"
+  option "without-rustls", "Compile without rustls and use native TLS provider"
+
+  depends_on "cmake" => :build
+  depends_on "rust" => :build
+  depends_on "ffmpeg" => :recommended
 
   def install
-    system "cargo", "install", *std_cargo_args
+    build_args = ["build", "--release"]
+    features = []
+    features.push("lang-cjk") if build.with? "cjk"
+    features.push("video-ffmpeg") if build.with? "ffmpeg"
+    features.push("native-tls") if build.without? "rustls"
+    build_args.push("--no-default-features") if build.without? "rustls"
+    build_args.push("--features=#{features.join(",")}") unless features.empty?
+
+    system "cargo", *build_args
+    cd "target/release" do
+      bin.install "gossip"
+      if build.with? "ffmpeg"
+        libexec.install Dir[shared_library("libSDL2*")]
+        libexec.install "libSDL2main.a"
+        bin.install_symlink Dir["#{libexec}/*"]
+      end
+    end
   end
 end
