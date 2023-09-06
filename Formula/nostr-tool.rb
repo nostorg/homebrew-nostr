fafc1e0ceee0921ff1b391de1f95780879b9a54e
1babo <1babo@proton.me>
2023-09-05 17:09:12 -0700

    nostr-tool 0.5.0

--- Formula/nostr-tool.rb
+++ Formula/nostr-tool.rb
@@ -2,8 +2,8 @@ class NostrTool < Formula
   desc "Simple CLI tool to send nostr events"
   homepage "https://github.com/0xtrr/nostr-tool"
   url "https://github.com/0xtrr/nostr-tool.git",
-      tag:      "0.4.0",
-      revision: "adecf08e69371b63b8a877385773dfa170559451"
+      tag:      "0.5.0",
+      revision: "e6ef6b5bac11a5bd51a8ec7e51c09184674b17ff"
   license "MIT"
   head "https://github.com/0xtrr/nostr-tool.git", branch: "master"
 
