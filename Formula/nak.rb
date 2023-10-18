582d1b174ebc4269a564690661f38557b45a54e7
git <badonyx@proton.me>
2023-10-18 12:58:06 -0700

    nak 0.0.9 (new formula)

---
+++ Formula/nak.rb
@@ -0,0 +1,19 @@
+class Nak < Formula
+  desc "Nostr Army Knife"
+  homepage "https://nak.nostr.com/"
+  url "https://github.com/fiatjaf/nak.git",
+      tag:      "v0.0.9",
+      revision: "459b127988faa226b33b366c3ae92204fdcdbd0f"
+  head "https://github.com/fiatjaf/nak.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    system "#{bin}/nak", "-h"
+  end
+end
