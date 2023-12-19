f1c9ae5a9f0f9e67576a3c986bbae456c6152fb0
git <badonyx@proton.me>
2023-12-18 17:11:28 -0800

    nak 0.1.5

--- Formula/nak.rb
+++ Formula/nak.rb
@@ -2,8 +2,8 @@ class Nak < Formula
   desc "Nostr Army Knife"
   homepage "https://nak.nostr.com/"
   url "https://github.com/fiatjaf/nak.git",
-      tag:      "v0.0.18",
-      revision: "f2f9dda33aeb40e205a0960996c945d006eef165"
+      tag:      "v0.1.5",
+      revision: "5415fd369cd49bb32e7c0850dd22f6fab962c7e3"
   head "https://github.com/fiatjaf/nak.git", branch: "master"
 
   depends_on "go" => :build
