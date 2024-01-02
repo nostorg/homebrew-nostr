6d40a4824af82c6b96ff3f1903f2ce0129dec77b
git <badonyx@proton.me>
2024-01-02 12:55:38 -0800

    gossip 0.9.0 (cask)

--- Casks/gossip.rb
+++ Casks/gossip.rb
@@ -1,9 +1,9 @@
 cask "gossip" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.8.2"
-  sha256 arm:   "460b5f449c1b8a15081b259acbd2799541f283d519e28f742c83a74a729317a4",
-         intel: "dc4dad4ce8c15fa5bd8ee9d040a12465dfd3bf80e4f311280b07a4360c120520"
+  version "0.9.0"
+  sha256 arm:   "063e209ed9bdba3b64c5d67e0be170883f35bb76df29e6a0922c23eb117a3431",
+         intel: "73f6708d70867f0e5cc68d55eaa2d81e49f385adb07d9c783552a8a69204f1e1"
 
   url "https://github.com/mikedilger/gossip/releases/download/v#{version}/gossip-#{version}-Darwin-#{arch}.dmg"
   name "Gossip"
