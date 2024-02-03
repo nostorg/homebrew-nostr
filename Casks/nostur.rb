47d5341dd79a1f6000544e341ddd97f357ff6ffe
Fabian <fabian@nostur.com>
2024-02-03 19:56:53 +0100

    nostur 1.12.0 (#14)

--- Casks/nostur.rb
+++ Casks/nostur.rb
@@ -1,6 +1,6 @@
 cask "nostur" do
-  version "1.11.0"
-  sha256 "b3cba18119050a274aef4857d8d49d1a005f0d5e508dfa5453b56eea5fb9a551"
+  version "1.12.0"
+  sha256 "e97ae9ffd23d2b6872c09ac11c7c5a56e2a2d090f8e13ad52872d00112996e19"
 
   url "https://nostur.com/Nostur-#{version}.dmg"
   name "Nostur"
