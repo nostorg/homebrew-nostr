801338b8d2d5c7e7b43ddf99f6e7b058e6158b1c
bad <badonyx@proton.me>
2024-04-08 11:23:49 -0700

    algia 0.0.69

--- Formula/algia.rb
+++ Formula/algia.rb
@@ -2,8 +2,8 @@ class Algia < Formula
   desc "Nostr CLI client written in Go"
   homepage "https://github.com/mattn/algia"
   url "https://github.com/mattn/algia.git",
-      tag:      "v0.0.64",
-      revision: "80dab4f0ef72af7ecccc01877f4c3056ebbba702"
+      tag:      "v0.0.69",
+      revision: "538ae3a656f5c01e10be673c5658d6765be4e10e"
   license "MIT"
   head "https://github.com/mattn/algia.git", branch: "master"
 
