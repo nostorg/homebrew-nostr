2c019941549861eee744af3ed32cfba65346d703
bad <badonyx@proton.me>
2024-06-04 12:20:32 -0700

    lume 4.0.8

--- Casks/lume.rb
+++ Casks/lume.rb
@@ -1,8 +1,11 @@
 cask "lume" do
-  version "3.0.1"
-  sha256 "6c4eda1e2a40bba35ee8b391c9df32d496c581bec9a63932309a163705160af0"
+  arch arm: "aarch64", intel: "x64"
 
-  url "https://github.com/luminous-devs/lume/releases/download/v#{version}/Lume_#{version}_universal.dmg",
+  version "4.0.8"
+  sha256 arm:   "6607153ead96aa4396486e40136a23edbc8273e00e3419f2acc5c24f7890816d",
+         intel: "a0bb2e3821f5517d69affd9084c9364f89e2713f853472b767e5901e11860dc4"
+
+  url "https://github.com/luminous-devs/lume/releases/download/v#{version}/Lume_#{version}_#{arch}.dmg",
       verified: "github.com/luminous-devs/lume/"
   name "Lume"
   desc "Ambitious nostr client"
