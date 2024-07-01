e6b71b1012b4e1473d8f6bedae55c9cc5f866440
bad <badonyx@proton.me>
2024-07-01 11:51:45 -0700

    lume 4.0.12

--- Casks/lume.rb
+++ Casks/lume.rb
@@ -1,9 +1,9 @@
 cask "lume" do
   arch arm: "aarch64", intel: "x64"
 
-  version "4.0.8"
-  sha256 arm:   "6607153ead96aa4396486e40136a23edbc8273e00e3419f2acc5c24f7890816d",
-         intel: "a0bb2e3821f5517d69affd9084c9364f89e2713f853472b767e5901e11860dc4"
+  version "4.0.12"
+  sha256 arm:   "f54730850cd958945768f5be663992001bbd4082c258c305faf91e59f17f3c8d",
+         intel: "52d8c42da60b336a21d39614b644b49cf4e58a01900868afa38cc5bace6c4a54"
 
   url "https://github.com/luminous-devs/lume/releases/download/v#{version}/Lume_#{version}_#{arch}.dmg",
       verified: "github.com/luminous-devs/lume/"
