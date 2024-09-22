1967f1db1bb54a8800e1da7dd0b1ee798bf115c9
bad <badonyx@proton.me>
2024-09-22 13:23:56 -0700

    nostr-tool 0.5.1

--- Formula/nostr-tool.rb
+++ Formula/nostr-tool.rb
@@ -2,8 +2,8 @@ class NostrTool < Formula
   desc "Simple CLI tool to send nostr events"
   homepage "https://github.com/0xtrr/nostr-tool"
   url "https://github.com/0xtrr/nostr-tool.git",
-      tag:      "0.5.0",
-      revision: "e6ef6b5bac11a5bd51a8ec7e51c09184674b17ff"
+      tag:      "0.5.1",
+      revision: "417ec05e068011f7b33a3e69cedb8d09ed99ba40"
   license "MIT"
   head "https://github.com/0xtrr/nostr-tool.git", branch: "master"
 
