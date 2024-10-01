d0f065c044ad6667b13e39debbeda5982414aacd
bad <badonyx@proton.me>
2024-09-30 21:47:07 -0700

    nostr-commander-rs 0.3.0

--- Formula/nostr-commander-rs.rb
+++ Formula/nostr-commander-rs.rb
@@ -2,8 +2,8 @@ class NostrCommanderRs < Formula
   desc "Simple but convenient CLI-based Nostr client app"
   homepage "https://github.com/8go/nostr-commander-rs"
   url "https://github.com/8go/nostr-commander-rs.git",
-      tag:      "v0.2.5",
-      revision: "590823dd96c70d876c892f3f47174049f744380c"
+      tag:      "v0.3.0",
+      revision: "94f3c005cf4f549d438aa760d8b88e406b9c4ccd"
   license "GPL-3.0"
   head "https://github.com/8go/nostr-commander-rs.git", branch: "master"
 
