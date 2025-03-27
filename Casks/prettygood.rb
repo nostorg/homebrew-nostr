36640c585fd1ee6f6af103217d99ae8f8b267ed5
bad <badonyx@proton.me>
2025-03-26 22:28:56 -0700

    test linux ci fix

--- Casks/prettygood.rb
+++ Casks/prettygood.rb
@@ -2,8 +2,11 @@ cask "prettygood" do
   arch arm: "-arm64"
 
   version "0.2.1-alpha"
-  sha256 arm:   "4a11737ddb6b916450cd04a023eb42211073d0f17bd004614fbffbbb0d9e0876",
-         intel: "d561d95df7b12109a5f435eecca87a1777c767442fc556d0b9941d25ca2fc17f"
+
+  on_macos do
+    sha256 arm:   "4a11737ddb6b916450cd04a023eb42211073d0f17bd004614fbffbbb0d9e0876",
+           intel: "d561d95df7b12109a5f435eecca87a1777c767442fc556d0b9941d25ca2fc17f"
+  end
 
   url "https://github.com/wds4/pretty-good/releases/download/v#{version}/PrettyGood-#{version}#{arch}.dmg"
   name "PrettyGood"
