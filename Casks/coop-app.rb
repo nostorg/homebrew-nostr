4efe08f74e3b5852a815b05a3120e3e590afc035
bad <badonyx@proton.me>
2026-01-02 22:42:19 +0000

    coop-app 0.3.0

--- Casks/coop-app.rb
+++ Casks/coop-app.rb
@@ -1,9 +1,9 @@
 cask "coop-app" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.2.11"
-  sha256 arm:          "8583a0aef04e44177f7389b6ac209437c5343bd9f982f00a7b8263fe86475809",
-         x86_64:       "7cb039637a23561e6d36c6528eed1fc54bd32220811314c5d791696149a2ebe5",
+  version "0.3.0"
+  sha256 arm:          "1cdb4ebff221392ddb9b6d234b7e91b719ef3be6d0a0e164164da6b4f4af4695",
+         x86_64:       "a6a46e12df75b44049cbb5645754554c562f49ddc6418e4e1ae676b9c8ec6a71",
          arm64_linux:  "0",
          x86_64_linux: "0"
 
@@ -18,7 +18,7 @@ cask "coop-app" do
     regex(/^v?(\d+(?:\.\d+)+(?:-\w+)?)$/i)
   end
 
-  depends_on arch: :arm64
+  depends_on macos: ">= :big_sur"
 
   app "Coop.app"
 
