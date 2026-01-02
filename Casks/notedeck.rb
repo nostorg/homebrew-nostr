11174297f6de0c45ad93a55e684dd88d4b0b9f5d
bad <badonyx@proton.me>
2026-01-02 23:09:51 +0000

    notedeck: fix ci - invalid sha256 value nil

--- Casks/notedeck.rb
+++ Casks/notedeck.rb
@@ -2,8 +2,10 @@ cask "notedeck" do
   arch arm: "arm64", intel: "x64"
 
   version "0.7.1"
-  sha256 arm:   "e97c654c824c047701721345801fd3e6161066763405a2fc58d71a813e27cb78",
-         intel: "a61d64323954bb0b65d603be7fd5c5c7f23de6c9188cbd35354824ce87e5d706"
+  sha256 arm:          "e97c654c824c047701721345801fd3e6161066763405a2fc58d71a813e27cb78",
+         intel:        "a61d64323954bb0b65d603be7fd5c5c7f23de6c9188cbd35354824ce87e5d706",
+         arm64_linux:  "0",
+         x86_64_linux: "0"
 
   url "https://damus.io/notedeck/dl/v#{version}/notedeck_v#{version}-#{arch}.dmg"
   name "Notedeck"
