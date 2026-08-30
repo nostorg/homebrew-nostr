bae9b15eb229bb0ea68ce90f4092de29f3c4a618
bad <badonyx@proton.me>
2026-08-30 13:26:05 -0700

    clean up empty hash

--- Casks/coop-app.rb
+++ Casks/coop-app.rb
@@ -2,10 +2,8 @@ cask "coop-app" do
   arch arm: "aarch64", intel: "x64"
 
   version "0.3.0"
-  sha256 arm:          "1cdb4ebff221392ddb9b6d234b7e91b719ef3be6d0a0e164164da6b4f4af4695",
-         x86_64:       "a6a46e12df75b44049cbb5645754554c562f49ddc6418e4e1ae676b9c8ec6a71",
-         arm64_linux:  "0",
-         x86_64_linux: "0"
+  sha256 arm:   "1cdb4ebff221392ddb9b6d234b7e91b719ef3be6d0a0e164164da6b4f4af4695",
+         intel: "a6a46e12df75b44049cbb5645754554c562f49ddc6418e4e1ae676b9c8ec6a71"
 
   url "https://github.com/lumehq/coop/releases/download/v#{version}/Coop_#{version.match(/[\d.]+/)}_#{arch}.dmg"
   name "Coop"
--- Casks/gossip-app.rb
+++ Casks/gossip-app.rb
@@ -2,10 +2,8 @@ cask "gossip-app" do
   arch arm: "arm64", intel: "x86_64"
 
   version "0.14.0"
-  sha256 arm:          "0165f5c2aab3f77aca8c4ad09f687363225d27ed35952b84d5251e228fe4ab07",
-         x86_64:       "2cb115e49cbcdeed901d9b1cacaaadfd56be0c747a06f6db3e4b283495c56506",
-         arm64_linux:  "0",
-         x86_64_linux: "0"
+  sha256 arm:   "0165f5c2aab3f77aca8c4ad09f687363225d27ed35952b84d5251e228fe4ab07",
+         intel: "2cb115e49cbcdeed901d9b1cacaaadfd56be0c747a06f6db3e4b283495c56506"
 
   url "https://github.com/mikedilger/gossip/releases/download/v#{version}/gossip-#{version}-Darwin-#{arch}.dmg"
   name "Gossip"
